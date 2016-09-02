187bbaacc001e051c0678802d760795b91a9304e
Manish Kumar Pal <manishpal.uee2015@iitr.ac.in>
2016-09-02 19:44:48 +0530

    class for students
    takes entry for a student and then shows the details

---
+++ 16.cpp
@@ -0,0 +1,30 @@
+#include<iostream>
+#include<cstring>
+
+using namespace std;
+
+class student {
+	private:
+		int rollNo;
+		char name[30];
+	public:
+		void getData(int roll_in, char *name_in) {
+			rollNo = roll_in;
+			strcpy(name, name_in);
+		}	
+		void putData() {
+			cout << "Roll No : " << rollNo << endl;
+			cout << "Name    : " << name << endl;
+		}
+};
+
+int main() {
+	student s1, s2;
+	s1.getData(1002, "Manish Kumar Pal");
+	s2.getData(1003, "Ankit Kumar Singh");
+	cout << "Student Details :\n";
+	s1.putData();
+	s2.putData();
+	system("pause");
+	return 0;
+}
