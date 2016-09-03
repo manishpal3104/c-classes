932ec7081334ba0f937b02f84e039b3c82fa03c8
Manish Kumar Pal <manishpal.uee2015@iitr.ac.in>
2016-09-03 10:42:27 +0530

    added an entry
    added a new student details

--- 16.cpp
+++ 16.cpp
@@ -22,6 +22,7 @@ int main() {
 	student s1, s2;
 	s1.getData(1002, "Manish Kumar Pal");
 	s2.getData(1003, "Ankit Kumar Singh");
+	s3.getData(1004, "Satish Kumar Pal");
 	cout << "Student Details :\n";
 	s1.putData();
 	s2.putData();
