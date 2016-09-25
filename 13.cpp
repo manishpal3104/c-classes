3145316ac769d483a19494534d2d7c2ae3529940
Manish Kumar Pal <manishpal.uee2015@iitr.ac.in>
2016-09-25 11:57:06 +0530

    added a new file
    added a new file

---
+++ 13.cpp
@@ -0,0 +1,26 @@
+#include<iostream>
+
+using namespace std;
+
+void Two(int&);
+void One();
+
+int main()
+{
+	int m = 5;
+	Two(m);
+	cout << m << endl;
+	One();
+	return 0;
+}
+void Two(int &x)
+{
+	x = 10;
+	cout << x << endl;
+}
+void One()
+{
+	int y = 1;
+	Two(y);
+	cout << y << endl;
+}
